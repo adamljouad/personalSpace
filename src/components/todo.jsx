f7d171dce964c040cc067f2f9d14704dad9c76c3
adamljouad <ad.ljouad@gmail.com>
2025-04-15 14:18:15 +0200

    Setup initial task input flow (name & date fields + add button)

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -4,18 +4,39 @@ import '../styles/todo.css'
 
 function TodoApp() {
   const [tasks, setTasks] = useState([]);
-  const [inputTask, setInputTask] = useState('');
+  const [inputName, setInputName] = useState('');
   const [inputDate, setInputDate] = useState('');
+
+  const addTasks = () => {
+    if (inputName === '') {
+      alert('Riempi i campi obbligatori')
+    } else {
+      setTasks((prev) => [...prev, {
+        name: inputName,
+        date: inputDate
+      }])
+    }
+  }
 
 
   return (
     <div className='todo-container'>
       <h2>Tasks</h2>
       <h3>Name</h3>
-      <input type='text' value={inputTask} onChange={(e) => setInputTask(e.target.value)}></input>
+      <input type='text' value={inputName} onChange={(e) => setInputName(e.target.value)}></input>
       <h3>Date</h3>
       <input type='date' value={inputDate} onChange={(e) => setInputDate(e.target.value)}></input>
+      <button onClick={addTasks}>Add Task</button>
       <p>This is your task list section.</p>
+      <ul>
+        {tasks.map((task, index) => {
+          return (
+            <li key={index}>
+              {task.name} - {task.date}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
