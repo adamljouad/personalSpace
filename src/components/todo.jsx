bad33b5468790e2f46d25e945b9825095430cd50
adamljouad <ad.ljouad@gmail.com>
2025-04-15 18:12:11 +0200

    Enhance TodoApp functionality by adding task filtering for Monday and Tuesday; update input field headings for clarity; improve task list structure in the render method.

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -7,6 +7,16 @@ function TodoApp() {
   const [inputName, setInputName] = useState('');
   const [inputDate, setInputDate] = useState('');
 
+  const getDay = (dateString) => {
+    const dateObj = new Date(dateString);
+    return dateObj.toLocaleDateString('en-US', { weekday: 'long' });
+  }
+
+  const mondayTask = tasks.filter(task => getDay(task.date) === 'Monday');
+  const tuesdayTask = tasks.filter(task => getDay(task.date) === 'Tuesday');
+
+  
+
   const addTasks = () => {
     if (inputName === '') {
       alert('Riempi i campi obbligatori')
@@ -15,6 +25,7 @@ function TodoApp() {
         name: inputName,
         date: inputDate
       }])
+      console.log(mondayTask)
     }
   }
 
@@ -22,21 +33,22 @@ function TodoApp() {
   return (
     <div className='todo-container'>
       <h2>Tasks</h2>
-      <h3>Name</h3>
+      <h4>Name</h4>
       <input type='text' value={inputName} onChange={(e) => setInputName(e.target.value)}></input>
-      <h3>Date</h3>
+      <h4>Date</h4>
       <input type='date' value={inputDate} onChange={(e) => setInputDate(e.target.value)}></input>
       <button onClick={addTasks}>Add Task</button>
-      <p>This is your task list section.</p>
-      <ul>
-        {tasks.map((task, index) => {
-          return (
-            <li key={index}>
-              {task.name} - {task.date}
-            </li>
-          )
-        })}
-      </ul>
+      <div className='todo-list'>
+        <ul>
+          {tasks.map((task, index) => {
+            return (
+              <li key={index}>
+                {task.name} - {task.date}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
